6049a552dcd4570a24f1667e07194b28cc2b4f79
Maxime <maximegrava@gmail.com>
2019-02-28 14:17:27 +0100

    created base files and simple css

--- HelpArc/HelpArcApp/urls.py
+++ HelpArc/HelpArcApp/urls.py
@@ -5,4 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('profile', views.profile, name='profile'),
+    path('askhelp', views.askhelp, name='askhelp'),
+    path('completeprofile', views.completeprofile, name='completeprofile'),
 ]
--- HelpArc/HelpArcApp/views.py
+++ HelpArc/HelpArcApp/views.py
@@ -6,3 +6,15 @@ from django.urls import reverse_lazy
 def index(request):
     context = {}
     return render(request, 'HelpArcApp/index.html', context)
+
+def profile(request):
+    context = {}
+    return render(request, 'HelpArcApp/profile.html', context)
+
+def askhelp(request):
+    context = {}
+    return render(request, 'HelpArcApp/askhelp.html', context)
+
+def completeprofile(request):
+    context = {}
+    return render(request, 'HelpArcApp/completeprofile.html', context)
