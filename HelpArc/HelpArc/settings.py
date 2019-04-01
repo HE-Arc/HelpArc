f1103d696790f42f94bd156725a0772b2e93f995
unknown <kevin.butikofer@he-arc.ch>
2019-04-01 22:21:20 +0200

    update setting static and urls

--- HelpArc/HelpArc/settings.py
+++ HelpArc/HelpArc/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = "/var/www/HelpArc/current/HelpArc/HelpArcApp/static/"
+STATIC_ROOT = "/var/www/static/"
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
